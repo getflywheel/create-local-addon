a9864841b5c8bacd6c59aef289e44ea724b71185
Ethan Bütt <ethan.butt@getflywheel.com>
2020-06-15 16:56:47 -0500

    docs: added process outline for basic generator functionality.

--- app/index.js
+++ app/index.js
@@ -3,9 +3,41 @@ const Generator = require('yeoman-generator');
 class LocalAddonGenerator extends Generator {
     constructor(args, opts) {
         super(args, opts);
+
+        //...
     }
 
+    // PRIVATE METHODS
+
     //...
+
+    // ORDERED GENERATOR STEPS
+
+    initializing() {
+        // print greeting, instructions, etc
+        // check Local installations
+        // check existing Local addons
+    }
+
+    prompting() {
+        // get addon name (if needed)
+        // confirm name availability
+    }
+
+    writing() {
+        // clone boilerplate, unpack, setup, etc
+    }
+
+    install() {
+       // symlink new addon (if needed)
+       // enable addon (if needed)
+    }
+
+    end() {
+        // clean up as needed
+        // confirm success/failure
+        // print next steps, links, etc
+    }
 }
 
 module.exports = LocalAddonGenerator;
